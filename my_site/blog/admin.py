95879625e7325fa3fc99579dbc3e72173af5d659
Mohamed Farag <mohamedfr320@gmail.com>
2024-02-13 19:27:45 +0200

    M1-P3-C

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','author','puplish','statues')
+    list_filter=('statues','created','puplish','author')
+    search_fields=('title','body')
+    prepopulated_fields={'slug':('title',)}
+    raw_id_fields=('author',)
+    date_hierarchy='puplish'
+    ordering=('statues','puplish')
 
 # Register your models here.
