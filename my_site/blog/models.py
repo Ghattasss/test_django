3e79178db4b285dfdff145e2f80d7cc4423162ae
Mohamed Farag <mohamedfr320@gmail.com>
2024-02-13 19:02:18 +0200

    M1-P3

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+class Post(models.Model):
+    class Statues(models.TextChoices):
+        DRAFT='DF','Draft'
+        PUPILISHED='PB','Pupilished'
 
-# Create your models here.
+    title=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=100)
+    body=models.TextField()
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    puplish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    statues=models.CharField(max_length=2,choices=Statues.choices,default=Statues.DRAFT)
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering=('-puplish',)
+        indexes=[models.Index(fields=['-puplish']),
+        ]
